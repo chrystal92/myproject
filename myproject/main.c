7f361f3e5d308b651ea3180c9a9f429a973a8d02
chrystal92 <chrystal92@163.com>
2016-05-17 08:41:31 +0800

    Initial Commit

---
+++ myproject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  myproject
+//
+//  Created by chrystal_zl on 16/5/17.
+//  Copyright © 2016年 chrystal_zl. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
